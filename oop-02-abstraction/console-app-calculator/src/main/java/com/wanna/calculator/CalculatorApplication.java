ec8554f28fe03002dffaafc148b07dd62c73d073
Your Name <your.email@example.com>
2025-03-21 23:18:40 +0700

    fix some issue for calculator

--- oop-02-abstraction/console-app-calculator/src/main/java/com/wanna/calculator/CalculatorApplication.java
+++ oop-02-abstraction/console-app-calculator/src/main/java/com/wanna/calculator/CalculatorApplication.java
@@ -1,9 +1,10 @@
 package com.wanna.calculator;
 
+import com.wanna.calculator.feature.AbstractCalculation;
 import com.wanna.calculator.feature.FeatureForAdding;
 import com.wanna.calculator.feature.FeatureForDivision;
 import com.wanna.calculator.feature.FeatureForMultiplication;
-import com.wanna.calculator.feature.FeatureForSubstaction;
+import com.wanna.calculator.feature.FeatureForSubstraction;
 import com.wanna.console.app.AbstractFeature;
 import com.wanna.console.app.ConsoleApplication;
 
@@ -12,9 +13,17 @@ public class CalculatorApplication {
 	public static void main(String[] args) {
 		var features = new AbstractFeature[] {
 				new FeatureForAdding(1),
-				new FeatureForSubstaction(2),
+				new FeatureForSubstraction(2),
 				new FeatureForMultiplication(3),
-				new FeatureForDivision(4)
+				new FeatureForDivision(4),
+				new AbstractCalculation(5,"Modulus Calculation") {
+					
+					@Override
+					public void doAddingAndShowResult(int first, int second) {
+						System.out.printf("%d %% %d = %d",first,second,first%second);
+						
+					}
+				}
 		};
 		
 		var app = new ConsoleApplication("Calculator App", features);
